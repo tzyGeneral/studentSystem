9772054ab67189d231ef229c80a8ad9e03d99fc1
zhenyuan <936451076@qq.com>
2020-04-16 20:19:35 +0800

    add：教师更改作业页面更新

--- src/view/correct.vue
+++ src/view/correct.vue
@@ -26,12 +26,25 @@ export default {
   data() {
       return {
           value: 3,
+          data: '',
           desc: ['很差', '不行', '一般', '不错', '很棒'],
           imgUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586886598672&di=f87a7c95609875f171f537e14a286ca0&imgtype=0&src=http%3A%2F%2F02imgmini.eastday.com%2Fmobile%2F20190130%2F20190130195010_b77d70fdf45834783b0a11b996991be4_1.jpeg'
       }
   },
+  mounted: function(){
+      this.response();
+  },
   methods: {
-
+      response(){
+        let that = this;
+        let token_data = that.$route.params.token;
+        let url = 'http://47.101.186.106:8000/main/teacher/getHomework';
+        axios.get(url, {params: {token: token_data}}).then(function(response){
+            let result = response.data.data;
+            that.data = result
+            console.log(result)
+        })
+      }
   },
 };
 </script>
